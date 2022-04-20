f4f1133c4a29550130c5cafa6562d02189d55eea
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-04-19 22:03:09 -0400

    update log_test 1.03

--- tests/log_test.py
+++ tests/log_test.py
@@ -1,7 +1,3 @@
-import os
 
-def test_log_file_exit(log):
-    """This makes the index page"""
-    info.log = log.get("info.log")
 
-assert os.path.exists(info.log) == True
+assert os.path.exists(log_file) == True
