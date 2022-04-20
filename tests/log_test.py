a8e9210ed450c67ca2931784e2a7ffc4305d16e8
kakatoto1 <98189795+kakatoto1@users.noreply.github.com>
2022-04-19 23:03:23 -0400

    update log_test 1.06

--- tests/log_test.py
+++ tests/log_test.py
@@ -1,4 +1,11 @@
 import os.path
+
 def log_test_exist():
-    log_file = ('info.log')
-assert os.path.exists(log_file) == True
+    # get root directory of project
+    root = os.path.dirname(os.path.abspath(__file__))
+    # set the name of the apps log folder to logs
+    logdir = os.path.join(root, 'logs')
+
+    log_file = os.path.join(logdir, 'info.log')
+
+    assert os.path.exists(log_file) == True
